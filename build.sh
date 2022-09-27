36e9ec301ae17fd3bb12171ec054bbc89f1fa9e2
Willem Olding <willemolding@gmail.com>
2022-09-26 22:42:08 -0400

    build working but endianess is wrong

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 
 # build for MIPS target and
 # also build the standard lib and a memory manager so there are no host requirements
+RUSTFLAGS="-C target-endian=big"
 cargo build --release -Z build-std=core,std,alloc,panic_abort,compiler_builtins -Z build-std-features=compiler-builtins-mem --target mipsel-unknown-none
 
 cp ./target/mipsel-unknown-none/release/rust-mips .
